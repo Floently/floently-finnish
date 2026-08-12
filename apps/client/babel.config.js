8a66b89ceb934762fa3988956dfa645e7a1ad9ee
Vitus Idi <79442436+galapoto@users.noreply.github.com>
2026-08-12 12:21:02 +0000

    Fix Expo 55 web export Babel config

--- apps/client/babel.config.js
+++ apps/client/babel.config.js
@@ -1,20 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './',
-            '@core': '../../packages/core',
-            '@ui': '../../packages/ui',
-          },
-        },
-      ],
-      'react-native-reanimated/plugin',
-    ],
   };
 };
